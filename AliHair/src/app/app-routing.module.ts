3a5e337a5a5fcec8d16aaacdc0107899691230c7
Pedro Barros <pedrokii.a.b@gmail.com>
2024-10-28 19:02:37 -0300

    Adicionando component principal e login

--- AliHair/src/app/app-routing.module.ts
+++ AliHair/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component'; // ajuste o caminho conforme necessário
+import { LoginComponent } from './login/login.component'; // ajuste o caminho conforme necessário
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/principal', pathMatch: 'full' }, // Redireciona para a página de login ao acessar a raiz
+  { path: 'login', component: LoginComponent },
+  { path: 'principal', component: PrincipalComponent },
+  // Adicione outras rotas aqui, se necessário
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AliHair/src/app/app.module.ts
+++ AliHair/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
